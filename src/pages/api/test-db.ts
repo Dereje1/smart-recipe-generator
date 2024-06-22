4ae8faa3924f6b665d8d956329de5054b9b8250e
Dereje Getahun <getahun.d@gmail.com>
2024-06-22 15:08:20 +0300

    setup and test mongodb connection

---
+++ src/pages/api/test-db.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import connectDB from '../../utils/db';
+
+type Data = {
+    message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    try {
+        await connectDB();
+        res.status(200).json({ message: 'Database connected successfully' });
+    } catch (error) {
+        console.dir({error})
+        res.status(500).json({ message: 'Database connection failed' });
+    }
+}
