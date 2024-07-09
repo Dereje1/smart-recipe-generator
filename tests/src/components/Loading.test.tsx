7640468f179678d68f3ec553db4a1a36300f5b3a
Dereje Getahun <getahun.d@gmail.com>
2024-07-09 14:49:47 +0300

    setup tests

---
+++ tests/src/components/Loading.test.tsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Loading from "../../../src/components/Loading";
+ 
+describe('The Loading spinner', () => {
+  it('renders', () => {
+    const { container } = render(<Loading />)
+    expect(container).toMatchSnapshot()
+  })
+})
