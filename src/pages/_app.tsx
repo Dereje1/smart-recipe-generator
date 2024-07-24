c9bb61c5a49b3c8910c43e2cf81a7752498d6f42
Dereje Getahun <getahun.d@gmail.com>
2024-07-24 14:52:20 +0300

    fix/add loading wheel on route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,36 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Loading from '../components/Loading';
 import Head from 'next/head'
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    return (
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const start = () => {
+            setLoading(true);
+        };
+        const end = () => {
+            setLoading(false);
+        };
+        router.events.on('routeChangeStart', start)
+        router.events.on('routeChangeComplete', end)
+        router.events.on('routeChangeError', end)
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method:
+        return () => {
+            router.events.off('routeChangeStart', start)
+            router.events.off('routeChangeComplete', end)
+            router.events.off('routeChangeError', end)
+        }
+    }, [router])
+
+    return loading ? <Loading /> : (
         <SessionProvider session={session}>
             <Layout>
                 <Head>
