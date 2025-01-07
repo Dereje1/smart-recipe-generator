fd582f60de2d5156b9cb1aae3e7b683867133191
Dereje Getahun <getahun.d@gmail.com>
2025-01-07 16:24:54 +0300

    fix/rendering issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { GetServerSideProps } from 'next';
 import { ClockIcon, FireIcon } from '@heroicons/react/24/solid';
 import SearchBar from '../components/SearchBar';
@@ -8,33 +8,54 @@ import { getFilteredRecipes, getServerSidePropsUtility, updateRecipeList, sortRe
 import { ExtendedRecipe } from '../types';
 
 const initialSearchView: ExtendedRecipe[] = []
+type latestRecipes = {
+    recipes: ExtendedRecipe[],
+    updateIndex: string | null
+}
 
 function Home({ recipes }: { recipes: ExtendedRecipe[] }) {
-    const [latestRecipes, setLatestRecipes] = useState(recipes);
+    const [latestRecipes, setLatestRecipes] = useState<latestRecipes>({
+        recipes,
+        updateIndex: null
+    });
     const [searchVal, setSearchVal] = useState('');
     const [searchView, setSearchView] = useState(initialSearchView);
     const [sortOption, setSortOption] = useState<'recent' | 'popular'>('popular'); // New state
 
+    // Create refs to store the *previous* values
+    const prevLatestRecipes = useRef(latestRecipes);
 
     useEffect(() => {
+        if (prevLatestRecipes.current !== latestRecipes) {
+            const newRecipe = latestRecipes.recipes.filter(r => r._id === latestRecipes.updateIndex)[0]
+            setSearchView(prevSearchView => updateRecipeList(prevSearchView, newRecipe));
+            prevLatestRecipes.current = latestRecipes;  // Updating after checking
+        }
+
         if (!searchVal.trim()) {
-            const resetView = sortRecipesHelper(latestRecipes, sortOption);
-            setSearchView(resetView);
+            setSearchView(prevSearchView => 
+                prevSearchView.length !== latestRecipes.recipes.length
+                    ? sortRecipesHelper(latestRecipes.recipes, sortOption)
+                    : prevSearchView
+            );
         }
     }, [searchVal, latestRecipes, sortOption]);
 
     const handleRecipeListUpdate = (recipe: ExtendedRecipe | null, deleteId?: string) => {
-        setLatestRecipes(updateRecipeList(latestRecipes, recipe, deleteId));
+        setLatestRecipes({
+            recipes: updateRecipeList(latestRecipes.recipes, recipe, deleteId),
+            updateIndex: recipe?._id || null
+        });
     };
 
     const handleSearch = () => {
-        const filteredRecipes = getFilteredRecipes(latestRecipes, searchVal.trim().toLowerCase());
+        const filteredRecipes = getFilteredRecipes(latestRecipes.recipes, searchVal.trim().toLowerCase());
         setSearchView(filteredRecipes);
     };
 
     const sortRecipes = (option: 'recent' | 'popular') => {
         if (sortOption === option) return; // Skip if already sorted
-        const sortedRecipes = sortRecipesHelper(searchView, option);
+        const sortedRecipes = sortRecipesHelper(latestRecipes.recipes, option);
         setSortOption(option);
         setSearchView(sortedRecipes); // Update the displayed recipes
     };
@@ -52,8 +73,8 @@ function Home({ recipes }: { recipes: ExtendedRecipe[] }) {
                 <button
                     onClick={() => sortRecipes('recent')}
                     className={`flex items-center px-4 py-2 rounded shadow-md transition duration-300 ${sortOption === 'recent'
-                            ? 'bg-green-500 text-white'
-                            : 'bg-gray-200 hover:bg-gray-300 hover:shadow-lg'
+                        ? 'bg-green-500 text-white'
+                        : 'bg-gray-200 hover:bg-gray-300 hover:shadow-lg'
                         }`}
                 >
                     <ClockIcon className="h-5 w-5 mr-2" />
@@ -62,8 +83,8 @@ function Home({ recipes }: { recipes: ExtendedRecipe[] }) {
                 <button
                     onClick={() => sortRecipes('popular')}
                     className={`flex items-center px-4 py-2 rounded shadow-md transition duration-300 ${sortOption === 'popular'
-                            ? 'bg-green-500 text-white'
-                            : 'bg-gray-200 hover:bg-gray-300 hover:shadow-lg'
+                        ? 'bg-green-500 text-white'
+                        : 'bg-gray-200 hover:bg-gray-300 hover:shadow-lg'
                         }`}
                 >
                     <FireIcon className="h-5 w-5 mr-2" />
