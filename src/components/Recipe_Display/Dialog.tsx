d8f91ab29d49c471e59c6abdaef1a4d7bfa9446e
Dereje Getahun <getahun.d@gmail.com>
2025-06-10 22:40:34 +0300

    fix: delete dialog z-index

--- src/components/Recipe_Display/Dialog.tsx
+++ src/components/Recipe_Display/Dialog.tsx
@@ -64,7 +64,7 @@ export default function RecipeDisplayModal({ isOpen, close, recipe, removeRecipe
 
     return (
         <>
-            <Dialog open={isOpen} as="div" className="relative z-50 focus:outline-none" onClose={close}>
+            <Dialog open={isOpen} as="div" className="relative z-100 focus:outline-none" onClose={close}>
                 <DialogBackdrop className="fixed inset-0 bg-black/50" />
                 <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                     <div className="flex min-h-full items-center justify-center p-4">
